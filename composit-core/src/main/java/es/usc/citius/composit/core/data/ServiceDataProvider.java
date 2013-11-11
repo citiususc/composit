ffacaa149ba125d7854a1a687d7a3ea2f8a63106
Pablo Rodríguez Mier <pablo.rodriguez.mier@usc.es>
2013-11-11 23:38:30 +0100

    ServiceDataProvider interface defined.

---
+++ composit-core/src/main/java/es/usc/citius/composit/core/data/ServiceDataProvider.java
@@ -0,0 +1,50 @@
+package es.usc.citius.composit.core.data;
+
+
+import es.usc.citius.composit.core.model.Operation;
+import es.usc.citius.composit.core.model.Service;
+
+import java.util.Set;
+
+public interface ServiceDataProvider<E> {
+    /**
+     * Locates and imports a service from the current service
+     * provider.
+     * @param serviceID unique ID of the service used to locate the resource.
+     * @return Imported service model.
+     */
+    Service<E> getService(String serviceID);
+
+    /**
+     * Locates and imports an operation from the current service provider.
+     * @param operationID unique ID of the operation used to locate the resource.
+     * @return Imported service model.
+     */
+    Operation<E> getOperation(String operationID);
+
+    /**
+     * Returns an iterator over the available operations.
+     * @return {@link Iterable} with the translated operations.
+     */
+    Iterable<Operation<E>> getOperations();
+
+    /**
+     * Returns an iterator over the available services.
+     * @return {@link Iterable} with the translated services.
+     */
+    Iterable<Service<E>> getServices();
+
+    /**
+     * List the available operation id's.
+     * @return Set of operation ids. To retrieve
+     * the operation model use {@link ServiceDataProvider#getOperation(String)}.
+     */
+    Set<String> listOperations();
+
+    /**
+     * List the available service id's.
+     * @return Set of service ids. To retrieve the service model
+     * use {@link ServiceDataProvider#getService(String)}.
+     */
+    Set<String> listServices();
+}
