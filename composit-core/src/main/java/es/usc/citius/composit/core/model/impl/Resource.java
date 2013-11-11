3fb361902f48ff213bdc58f4e94fe7ac304dddee
Pablo Rodríguez Mier <pablo.rodriguez.mier@usc.es>
2013-11-11 22:47:37 +0100

    Resource class updated. Constructor with URI added.

--- composit-core/src/main/java/es/usc/citius/composit/core/model/impl/Resource.java
+++ composit-core/src/main/java/es/usc/citius/composit/core/model/impl/Resource.java
@@ -4,29 +4,38 @@ import java.io.Serializable;
 import java.net.URI;
 
 public class Resource implements Serializable{
-    // Name cannot be null
+    // ID cannot be null. The ID is used to identify unique instances of resource.
     private final String id;
-    private final URI uri = null;
+    private final URI uri;
 
     public Resource(String id) {
         this.id = id;
+        this.uri = null;
+    }
+
+    public Resource(String id, URI uri) {
+        this.id = id;
+        this.uri = uri;
     }
 
     @Override
     public boolean equals(Object o) {
         if (this == o) return true;
-        if (!(o instanceof Resource)) return false;
+        if (o == null || getClass() != o.getClass()) return false;
 
         Resource resource = (Resource) o;
 
         if (!id.equals(resource.id)) return false;
+        if (uri != null ? !uri.equals(resource.uri) : resource.uri != null) return false;
 
         return true;
     }
 
     @Override
     public int hashCode() {
-        return id.hashCode();
+        int result = id.hashCode();
+        result = 31 * result + (uri != null ? uri.hashCode() : 0);
+        return result;
     }
 
     public String getId() {
@@ -35,5 +44,10 @@ public class Resource implements Serializable{
 
     public URI getUri() {
         return uri;
+    }
+
+    @Override
+    public String toString() {
+        return id;
     }
 }
