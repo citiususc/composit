ae96ca5769af1281af65030dae3f197753caf618
Pablo Rodríguez Mier <pablo.rodriguez.mier@usc.es>
2013-11-12 20:49:55 +0100

    Util methods to get source/target elements of a match implemented in SetMatchFunction.SetMatchResult

--- composit-core/src/main/java/es/usc/citius/composit/core/matcher/SetMatchFunction.java
+++ composit-core/src/main/java/es/usc/citius/composit/core/matcher/SetMatchFunction.java
@@ -1,11 +1,11 @@
 package es.usc.citius.composit.core.matcher;
 
-import com.google.common.collect.HashBasedTable;
-import com.google.common.collect.ImmutableTable;
-import com.google.common.collect.Table;
+import com.google.common.base.Functions;
+import com.google.common.collect.*;
 
 import java.util.Map;
 import java.util.Set;
+import java.util.SortedSet;
 
 /**
  * This interface defines the main methods for matching sets of elements.
@@ -25,7 +25,21 @@ public interface SetMatchFunction<E,T extends Comparable<T>> extends MatchFuncti
             return ImmutableTable.copyOf(matchTable);
         }
 
-        // TODO; Implement util methods to get all matches sorted by type, etc.
+        public Set<E> getSourceElements(){
+            return getMatchTable().rowKeySet();
+        }
+
+        public Set<E> getTargetElements(){
+            return getMatchTable().columnKeySet();
+        }
+
+        public SortedSet<E> getSourceElementsThatMatch(E targetElement){
+            // Get the map with matcher -> type
+            Map<E,T> sourceElements = this.getMatchTable().column(targetElement);
+            // Order elements by the match type values
+            Ordering<E> matchTypeComparator = Ordering.natural().onResultOf(Functions.forMap(sourceElements));
+            return ImmutableSortedMap.copyOf(sourceElements, matchTypeComparator).keySet();
+        }
 
     }
     /**
