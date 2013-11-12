09c89f5467600e8015be949eef53a5945d25e3a8
Pablo Rodríguez Mier <pablo.rodriguez.mier@usc.es>
2013-11-12 20:49:18 +0100

    MatchFunction description updated

--- composit-core/src/main/java/es/usc/citius/composit/core/matcher/MatchFunction.java
+++ composit-core/src/main/java/es/usc/citius/composit/core/matcher/MatchFunction.java
@@ -4,5 +4,11 @@ package es.usc.citius.composit.core.matcher;
  * @author Pablo Rodríguez Mier <<a href="mailto:pablo.rodriguez.mier@usc.es">pablo.rodriguez.mier@usc.es</a>>
  */
 public interface MatchFunction<E,T extends Comparable<T>> {
+    /**
+     * Return the match type between source and target elements.
+     * @param source source element to match
+     * @param target target element to be matched
+     * @return match type or null if the match does not exist or cannot be computed.
+     */
     T match(E source, E target);
 }
