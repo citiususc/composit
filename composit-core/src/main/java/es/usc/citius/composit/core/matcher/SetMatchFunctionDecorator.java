34ef469abfb8e155c8153a3582875c4206f4b203
Pablo Rodríguez Mier <pablo.rodriguez.mier@usc.es>
2013-11-12 22:37:31 +0100

    SetMatchFunctionDecorator impl and test added.

---
+++ composit-core/src/main/java/es/usc/citius/composit/core/matcher/SetMatchFunctionDecorator.java
@@ -0,0 +1,44 @@
+package es.usc.citius.composit.core.matcher;
+
+
+import java.util.Set;
+
+public class SetMatchFunctionDecorator<E,T extends Comparable<T>> implements SetMatchFunction<E,T> {
+    private MatchFunction<E,T> decoratedMatcher;
+
+    public SetMatchFunctionDecorator(MatchFunction<E, T> decoratedMatcher) {
+        this.decoratedMatcher = decoratedMatcher;
+    }
+
+    private SetMatchResult<E, T> match(Set<E> source, Set<E> target, boolean full){
+        // Compute partial match using the decorated match function
+        SetMatchResult<E,T> result = new SetMatchResult<E, T>();
+        for(E y : target){
+            for(E x : source){
+                T matchType = decoratedMatcher.match(x,y);
+                if (matchType != null){
+                    result.addMatch(x,y,matchType);
+                    // We found a match between x and y.
+                    // Skip to the next target element
+                    if (!full) break;
+                }
+            }
+        }
+        return result;
+    }
+
+    @Override
+    public SetMatchResult<E, T> partialMatch(Set<E> source, Set<E> target) {
+        return match(source, target, false);
+    }
+
+    @Override
+    public SetMatchResult<E, T> fullMatch(Set<E> source, Set<E> target) {
+        return match(source, target, true);
+    }
+
+    @Override
+    public T match(E source, E target) {
+        return decoratedMatcher.match(source, target);
+    }
+}
---
+++ composit-core/src/test/java/es/usc/citius/composit/core/matcher/SetMatchFunctionDecoratorTest.java
@@ -0,0 +1,77 @@
+package es.usc.citius.composit.core.matcher;
+
+import com.google.common.collect.Sets;
+import org.junit.Before;
+import org.junit.Test;
+import org.mockito.Mockito;
+
+import java.util.Set;
+import java.util.SortedSet;
+
+import static junit.framework.Assert.assertNotNull;
+import static junit.framework.Assert.assertTrue;
+import static org.junit.Assert.assertEquals;
+import static org.mockito.Mockito.when;
+
+public class SetMatchFunctionDecoratorTest {
+
+    private MatchFunction<String,Integer> mockedMatcher;
+    private SetMatchFunctionDecorator<String,Integer> setMatcher;
+    private Set<String> source = Sets.newHashSet("a", "b", "c");
+    private Set<String> target = Sets.newHashSet("d", "e", "f");
+
+    @Before
+    public void setUp(){
+        // Source elements: a, b, c
+        // Target elements: d, e
+
+        mockedMatcher = Mockito.mock(MatchFunction.class);
+        setMatcher = new SetMatchFunctionDecorator<String, Integer>(mockedMatcher);
+
+        // Configure matcher behaviour
+        when(mockedMatcher.match("a","d")).thenReturn(1);
+        when(mockedMatcher.match("a","e")).thenReturn(2);
+
+        when(mockedMatcher.match("b","d")).thenReturn(0);
+        when(mockedMatcher.match("b","e")).thenReturn(4);
+
+        when(mockedMatcher.match("c","e")).thenReturn(5);
+    }
+    @Test
+    public void testPartialMatch() throws Exception {
+        SetMatchResult<String, Integer> result = setMatcher.partialMatch(source, target);
+        // Only d and e matched
+        assertEquals(Sets.newHashSet("d", "e"), result.getTargetElements());
+        // Just the first element recorded
+        assertEquals(1, result.getSourceElementsThatMatch("d").size());
+        assertEquals(1, result.getSourceElementsThatMatch("e").size());
+        assertNotNull(result.getSourceElementsThatMatch("f"));
+        assertTrue(result.getSourceElementsThatMatch("f").isEmpty());
+    }
+
+    @Test
+    public void testFullMatch() throws Exception {
+        SetMatchResult<String, Integer> result = setMatcher.fullMatch(source, target);
+        // Only d and e matched
+        assertEquals(Sets.newHashSet("d", "e"), result.getTargetElements());
+        // Only a and b match d
+        assertEquals(Sets.newHashSet("a", "b"), result.getSourceElementsThatMatch("d"));
+        // a, b and c match e
+        assertEquals(source, result.getSourceElementsThatMatch("e"));
+        assertNotNull(result.getSourceElementsThatMatch("f"));
+        assertTrue(result.getSourceElementsThatMatch("f").isEmpty());
+    }
+
+    @Test
+    public void testSortedTypesOnFullMatch(){
+        SetMatchResult<String, Integer> result = setMatcher.fullMatch(source, target);
+        SortedSet<String> elements = result.getSourceElementsThatMatch("e");
+        assertEquals("a",elements.first());
+        assertEquals("c",elements.last());
+    }
+
+    @Test
+    public void testMatch() throws Exception {
+        assertEquals(1, setMatcher.match("a","d").intValue());
+    }
+}
