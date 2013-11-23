ff0a6b9a6a46c737b0b3d4f4a10400c2776a72ed
Pablo Rodríguez Mier <pablo.rodriguez.mier@usc.es>
2013-11-23 12:17:48 +0100

    WSC01 Forward discovery test added

--- composit-test/src/test/java/es/usc/citius/composit/test/composition/ForwardServiceDiscovererWSC01.java
+++ composit-test/src/test/java/es/usc/citius/composit/test/composition/ForwardServiceDiscovererWSC01.java
@@ -1,35 +1,75 @@
 package es.usc.citius.composit.test.composition;
 
+import com.google.common.base.Stopwatch;
 import es.usc.citius.composit.core.composition.DiscoveryIO;
 import es.usc.citius.composit.core.composition.MatchBasedDiscoveryIO;
 import es.usc.citius.composit.core.composition.search.ForwardServiceDiscoverer;
 import es.usc.citius.composit.core.knowledge.Concept;
-import es.usc.citius.composit.core.provider.IndexedServiceProvider;
+import es.usc.citius.composit.core.provider.MemoryIndexServiceProvider;
 import es.usc.citius.composit.core.provider.ServiceDataProvider;
 import es.usc.citius.composit.wsc08.data.WSCTest;
 import es.usc.citius.composit.wsc08.data.matcher.ExactPluginKnowledgeBaseMatchGraph;
 import org.junit.Before;
 import org.junit.Test;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import java.io.IOException;
 
 /**
  * @author Pablo Rodríguez Mier <<a href="mailto:pablo.rodriguez.mier@usc.es">pablo.rodriguez.mier@usc.es</a>>
  */
 public class ForwardServiceDiscovererWSC01 {
-    private ForwardServiceDiscoverer<Concept> forwardSearch;
-    private WSCTest.Dataset dataset;
+    private static final Logger log = LoggerFactory.getLogger(ForwardServiceDiscovererWSC01.class);
 
-    @Before
-    public void setUp() throws Exception {
-        dataset = WSCTest.TESTSET_2008_01.dataset();
-        ServiceDataProvider<Concept> provider = new IndexedServiceProvider<Concept>(dataset.getServiceProvider());
+    private void run(WSCTest test) throws IOException {
+        WSCTest.Dataset dataset = test.dataset();
+        ServiceDataProvider<Concept> provider = new MemoryIndexServiceProvider<Concept>(dataset.getServiceProvider());
         // Create a I/O Discovery and the match graph
         ExactPluginKnowledgeBaseMatchGraph matchGraph = new ExactPluginKnowledgeBaseMatchGraph(dataset.getKb());
         DiscoveryIO<Concept> discovery = new MatchBasedDiscoveryIO<Concept, Boolean>(matchGraph, provider);
-        forwardSearch = new ForwardServiceDiscoverer<Concept>(discovery, matchGraph);
+        Stopwatch w = Stopwatch.createStarted();
+        new ForwardServiceDiscoverer<Concept>(discovery, matchGraph).search(dataset.getRequest());
+        log.debug("Total time {}", w.stop().toString());
     }
 
     @Test
     public void testWSC01() throws Exception {
-        forwardSearch.search(dataset.getRequest());
+        run(WSCTest.TESTSET_2008_01);
+    }
+
+    @Test
+    public void testWSC02() throws Exception {
+        run(WSCTest.TESTSET_2008_02);
+    }
+
+    @Test
+    public void testWSC03() throws Exception {
+        run(WSCTest.TESTSET_2008_03);
+    }
+
+    @Test
+    public void testWSC04() throws Exception {
+        run(WSCTest.TESTSET_2008_04);
+    }
+
+    @Test
+    public void testWSC05() throws Exception {
+        run(WSCTest.TESTSET_2008_05);
+    }
+
+    @Test
+    public void testWSC06() throws Exception {
+        run(WSCTest.TESTSET_2008_06);
+    }
+
+    @Test
+    public void testWSC07() throws Exception {
+        run(WSCTest.TESTSET_2008_07);
+    }
+
+    @Test
+    public void testWSC08() throws Exception {
+        run(WSCTest.TESTSET_2008_08);
     }
 }
