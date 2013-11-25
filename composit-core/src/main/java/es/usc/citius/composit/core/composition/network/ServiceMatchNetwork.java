2c649c4c8715cbd9830c1ff242bc2bf28d357517
pablo.rodriguez.mier <pablo.rodriguez.mier@usc.es>
2013-11-25 13:20:02 +0100

    TODO added to ServiceMatchNetwork. Better functions to retrieve matched operations must be eventually implemented.

--- composit-core/src/main/java/es/usc/citius/composit/core/composition/network/ServiceMatchNetwork.java
+++ composit-core/src/main/java/es/usc/citius/composit/core/composition/network/ServiceMatchNetwork.java
@@ -5,4 +5,5 @@ import es.usc.citius.composit.core.composition.LeveledServices;
 import es.usc.citius.composit.core.matcher.graph.MatchGraph;
 
 public interface ServiceMatchNetwork<E,T extends Comparable<T>> extends LeveledServices<E>, MatchGraph<E,T> {
+    // TODO; Add methods to retrieve source/target operation match
 }
