4b5885a94a1e9c07b3a62df6e120d76842f4947b
pablo.rodriguez.mier <pablo.rodriguez.mier@usc.es>
2013-11-27 18:24:40 +0100

    ServiceMatchNetwork javadoc changes. Image added.

--- composit-core/src/main/java/es/usc/citius/composit/core/composition/network/ServiceMatchNetwork.java
+++ composit-core/src/main/java/es/usc/citius/composit/core/composition/network/ServiceMatchNetwork.java
@@ -21,6 +21,21 @@ package es.usc.citius.composit.core.composition.network;
 import es.usc.citius.composit.core.composition.LeveledServices;
 import es.usc.citius.composit.core.matcher.graph.MatchGraph;
 
+/**
+ * <p>A Service Match Network (SMN) represents the full directed acyclic composition graph model. This network
+ * consists of two well-differentiated parts: a leveled structure with multiple layers
+ * that correspond with the levels (or layers) of the directed acyclic graph ({@link LeveledServices}),
+ * and the match relations between concepts ({@link MatchGraph}). A Service Match Network contains all
+ * the information needed to search for optimal compositions.</p>
+ *
+ * <center><img src="../../../../../../../../assets/images/javadoc/ServiceMatchNetwork.png"></img></center>
+ *
+ * <p>There is a default implementation called {@link HashServiceMatchNetwork} that holds all the information
+ * in memory using hash maps.</p>
+ *
+ * @param <E> type of the inputs/outputs of the services.
+ * @param <T> type of a match between inputs and outputs.
+ */
 public interface ServiceMatchNetwork<E,T extends Comparable<T>> extends LeveledServices<E>, MatchGraph<E,T> {
     // TODO; Add methods to retrieve source/target operation match
 }
