b2051292906fe4d8294a98db9882f66e1aae1369
pablo.rodriguez.mier <pablo.rodriguez.mier@usc.es>
2013-11-27 19:02:39 +0100

    Operation javadoc updated

--- composit-core/src/main/java/es/usc/citius/composit/core/model/Operation.java
+++ composit-core/src/main/java/es/usc/citius/composit/core/model/Operation.java
@@ -19,15 +19,28 @@ package es.usc.citius.composit.core.model;
 
 
 /**
- * Generic IOPE specification of a service operation. A service operation is basically
- * a {@link Signature} with the information about inputs, outputs, preconditions and effects,
- * and the {@link Service} owner.
+ * Generic IO(PE) specification of a service operation. A service operation is basically
+ * a {@link Signature} with the information about inputs, outputs, the PE part (preconditions and effects),
+ * and the {@link Service} owner. NOTE: Preconditions/Effects are not supported in the current version of
+ * the API.
  *
  * @author Pablo Rodríguez Mier <<a href="mailto:pablo.rodriguez.mier@usc.es">pablo.rodriguez.mier@usc.es</a>>
- * @param <E> data type.
+ * @param <E> input/output type.
  */
 public interface Operation<E> extends Resource {
+
+    /**
+     * Get the service of this operation. The service can be null (for example, dummy services
+     * has no services associated).
+     *
+     * @return Service owner.
+     */
     Service<E> getServiceOwner();
+
+    /**
+     * Signature (inputs/outputs) of the operation.
+     * @return Signature with the inputs and outputs.
+     */
     Signature<E> getSignature();
     //TODO; implement precondition / effects methods
 }
