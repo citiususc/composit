d90400c8c0785d8052b395e97466678d4c26749f
pablo.rodriguez.mier <pablo.rodriguez.mier@usc.es>
2013-12-27 13:08:17 +0100

    Forward+Backward random query search strategy

--- composit-cli/src/main/java/es/usc/citius/composit/cli/command/RandomQueryCommand.java
+++ composit-cli/src/main/java/es/usc/citius/composit/cli/command/RandomQueryCommand.java
@@ -20,6 +20,7 @@ package es.usc.citius.composit.cli.command;
 import com.beust.jcommander.Parameter;
 import com.beust.jcommander.Parameters;
 import com.google.common.collect.ImmutableList;
+import com.google.common.collect.Sets;
 import es.usc.citius.composit.cli.CompositCli;
 import es.usc.citius.composit.core.composition.InputDiscoverer;
 import es.usc.citius.composit.core.composition.LeveledServices;
@@ -27,9 +28,12 @@ import es.usc.citius.composit.core.composition.network.ServiceMatchNetwork;
 import es.usc.citius.composit.core.composition.optimization.BackwardMinimizationOptimizer;
 import es.usc.citius.composit.core.composition.optimization.FunctionalDominanceOptimizer;
 import es.usc.citius.composit.core.composition.search.ComposIT;
+import es.usc.citius.composit.core.composition.search.CompositionProblem;
 import es.usc.citius.composit.core.composition.search.ForwardServiceDiscoverer;
 import es.usc.citius.composit.core.knowledge.Concept;
+import es.usc.citius.composit.core.matcher.graph.MatchGraph;
 import es.usc.citius.composit.core.model.Operation;
+import es.usc.citius.composit.core.model.Operations;
 import es.usc.citius.composit.core.model.impl.SignatureIO;
 import es.usc.citius.composit.wsc08.data.WSCTest;
 
@@ -54,55 +58,236 @@ public class RandomQueryCommand implements CliCommand {
     @Override
     public void invoke(CompositCli contextCli) throws Exception {
         this.cli = contextCli;
-
         WSCTest.Dataset dataset = test.dataset();
+        backwardSearch(dataset);
+    }
+
+    private void backwardSearch(WSCTest.Dataset dataset){
+        // Start from the outputs
         Random r = new Random();
+        int totalOutputs = r.nextInt(9)+1;
+        Set<Concept> initialOutputs = new HashSet<Concept>();
+        for(int i=0; i < totalOutputs; i++){
+            initialOutputs.add(randomElement(dataset.getMatchGraph().getElements(), r));
+        }
+        //initialOutputs.clear();
+        //initialOutputs.add(dataset.getKb().getConcept("con896546722"));
+        Set<Concept> selectedOutputs = new HashSet<Concept>(initialOutputs);
+        cli.println(totalOutputs + " wanted outputs.");
+        // Start backwards
+        InputDiscoverer<Concept> discoverer = dataset.getDefaultCompositionProblem().getInputDiscoverer();
         Set<Operation<Concept>> selected = new HashSet<Operation<Concept>>();
-        Set<Concept> inputs = new HashSet<Concept>();
-        Set<String> availableOps = dataset.getServiceProvider().listOperations();
-        String randomService = randomElement(availableOps, r);
-        cli.println("Random operation selected: " + randomService);
-        Operation<Concept> operation = dataset.getServiceProvider().getOperation(randomService);
-        inputs.addAll(operation.getSignature().getInputs());
-        // Generate all possible levels
+        List<Set<Operation<Concept>>> levels = new LinkedList<Set<Operation<Concept>>>();
+        Set<Concept> unsolved = new HashSet<Concept>();
+
+        do {
+            Set<Operation<Concept>> levelOps = new HashSet<Operation<Concept>>();
+            for(Concept output : selectedOutputs){
+                Set<Operation<Concept>> ops = discoverer.findOperationsConsuming(output);
+                ops.removeAll(selected);
+                if (ops.isEmpty()){
+                    unsolved.add(output);
+                } else {
+                    // Select one randomly
+                    Operation<Concept> op = randomElement(ops, r);
+                    ops = new HashSet<Operation<Concept>>(Arrays.asList(op));
+                    selected.addAll(ops);
+                    levelOps.addAll(ops);
+                }
+            }
+            if (levelOps.isEmpty()){
+                break;
+            }
+            levels.add(levelOps);
+            cli.println(levelOps.size() + " added");
+            // Get the new inputs
+            selectedOutputs = Operations.inputs(levelOps);
+            selectedOutputs.removeAll(unsolved);
+        } while (true);
+        cli.println(unsolved.size() + " unsolved inputs.");
+        /*
+        ForwardServiceDiscoverer<Concept, Boolean> fs = new ForwardServiceDiscoverer<Concept, Boolean>(dataset.getDefaultCompositionProblem().getInputDiscoverer(),
+                dataset.getDefaultCompositionProblem().getMatchGraph());
+        */
+
+        // Use the last outputs as inputs
+        Set<Concept> selectedInputs = selectedOutputs;
+        cli.println("Forward search with " + selectedInputs.size() + " inputs and " + initialOutputs.size() + " outputs.");
+        //fs.search(new SignatureIO<Concept>(unsolved, Collections.<Concept>emptySet()));
+        forwardSearch(selectedInputs, initialOutputs, discoverer, dataset.getMatchGraph(), r);
+    }
+
+    private void forwardSearch(Set<Concept> availableInputs, Set<Concept> wantedOutputs, final InputDiscoverer<Concept> discoverer, final MatchGraph<Concept, Boolean> matcher, Random r){
+        ForwardServiceDiscoverer<Concept, Boolean> fs = new ForwardServiceDiscoverer<Concept, Boolean>(discoverer,
+                matcher);
+        // Inputs and outputs of the random request
+        Set<Concept> inputs = new HashSet<Concept>(availableInputs);
+        // Generate the reachability set of operations
+        fs.setRelaxedMatchCondition(true);
+        ServiceMatchNetwork<Concept, Boolean> reachabilityGraph =
+                fs.search(new SignatureIO<Concept>(inputs, Collections.<Concept>emptySet()));
+        fs.setRelaxedMatchCondition(false);
+        Set<Operation<Concept>> reachableOps =
+                Sets.newHashSet(reachabilityGraph.getOperations());
+
+        int levels;
+        cli.println("Starting...");
+        ServiceMatchNetwork<Concept, Boolean> graph;
+
+        // Extend the initial graph
+        do {
+            fs = new ForwardServiceDiscoverer<Concept, Boolean>(discoverer,
+                    matcher);
+            graph = fs.search(new SignatureIO<Concept>(inputs, Collections.<Concept>emptySet()));
+            levels = graph.numberOfLevels()-2;
+            Map<Operation<Concept>, Set<Concept>> unmatchedInputMap = fs.getUnmatchedInputMap();
+
+            int minSize = Integer.MAX_VALUE;
+            Operation<Concept> op = null;
+            // Find the operation with less unmatched inputs (different from 0)
+            for(Map.Entry<Operation<Concept>, Set<Concept>> entry : unmatchedInputMap.entrySet()){
+                int currentSize = entry.getValue().size();
+                if (currentSize != 0 && currentSize < minSize){
+                    minSize = currentSize;
+                    op = entry.getKey();
+                }
+            }
+            Set<Concept> newInputs = new HashSet<Concept>();
+            if (op == null){
+                // Select a random operation
+                Set<Operation<Concept>> invokableOps =
+                        Sets.newHashSet(graph.getOperations());
+                reachableOps.removeAll(invokableOps);
+                if (reachableOps.isEmpty()) break;
+                op = randomElement(reachableOps, r);
+                Set<Concept> generatedOutputs = Operations.outputs(invokableOps);
+                Set<Concept> matched = matcher.partialMatch(generatedOutputs, op.getSignature().getInputs()).getTargetElements();
+                newInputs.addAll(new HashSet<Concept>(Sets.difference(op.getSignature().getInputs(), matched)));
+            } else {
+                newInputs.addAll(unmatchedInputMap.get(op));
+            }
+
+            // Compute the minimum number of inputs that we need to add in order to make the
+            // operation invokable:
+            inputs.addAll(newInputs);
+        } while (levels < minLevels);
+
+
+        wantedOutputs = Operations.outputs(graph.getLeveledList().get(graph.numberOfLevels()-2));
+        // Generate optimal composition
+        cli.println("Total inputs: " + inputs.size());
+        cli.println("Total outputs: " + wantedOutputs.size());
+        ComposIT<Concept, Boolean> composit = new ComposIT<Concept, Boolean>(new CompositionProblem<Concept, Boolean>() {
+            @Override
+            public MatchGraph<Concept, Boolean> getMatchGraph() {
+                return matcher;
+            }
+
+            @Override
+            public InputDiscoverer<Concept> getInputDiscoverer() {
+                return discoverer;
+            }
+        });
+        composit.addOptimization(new BackwardMinimizationOptimizer<Concept, Boolean>());
+        composit.addOptimization(new FunctionalDominanceOptimizer<Concept, Boolean>());
+        composit.search(new SignatureIO<Concept>(inputs, wantedOutputs));
+    }
+
+    private void forwardSearch(WSCTest.Dataset dataset){
+        Random r = new Random();
+        MatchGraph<Concept, Boolean> matcher = dataset.getDefaultCompositionProblem().getMatchGraph();
         InputDiscoverer<Concept> discoverer = dataset.getDefaultCompositionProblem().getInputDiscoverer();
         ForwardServiceDiscoverer<Concept, Boolean> fs = new ForwardServiceDiscoverer<Concept, Boolean>(dataset.getDefaultCompositionProblem().getInputDiscoverer(),
                 dataset.getDefaultCompositionProblem().getMatchGraph());
-        ServiceMatchNetwork<Concept, Boolean> result = fs.search(new SignatureIO<Concept>(inputs, Collections.<Concept>emptySet()));
-        int levels = result.numberOfLevels()-2;
-        cli.println("Number of levels: " + levels);
+        // Selected operations
+        Set<Operation<Concept>> selected = new HashSet<Operation<Concept>>();
+        // Inputs and outputs of the random request
+        Set<Concept> inputs = new HashSet<Concept>();
+        Set<String> availableOps = dataset.getServiceProvider().listOperations();
+        // Select a service that will be placed in the first layer:
+        String randomService = randomElement(availableOps, r);
+        cli.println("Random operation selected: " + randomService);
+        Operation<Concept> operation = dataset.getServiceProvider().getOperation(randomService);
         selected.add(operation);
+        // Make it invokable
+        inputs.addAll(operation.getSignature().getInputs());
+        // Generate the reachability set of operations
+        fs.setRelaxedMatchCondition(true);
+        ServiceMatchNetwork<Concept, Boolean> reachabilityGraph =
+                fs.search(new SignatureIO<Concept>(inputs, Collections.<Concept>emptySet()));
+        fs.setRelaxedMatchCondition(false);
+        Set<Operation<Concept>> reachableOps =
+                Sets.newHashSet(reachabilityGraph.getOperations());
 
-        while(levels < minLevels){
-            // Select a new random operation for a random level
-            Set<Operation<Concept>> operations = findNewOperations(discoverer, result, selected);
-            if (operations.isEmpty()) break;
+        int levels;
+        cli.println("Starting...");
+        ServiceMatchNetwork<Concept, Boolean> graph;
 
-            Operation<Concept> op = randomElement(operations, r);
-            selected.add(op);
-            // Make it executable
-            inputs.addAll(op.getSignature().getInputs());
-            // Repeat the search
-            result = fs.search(new SignatureIO<Concept>(inputs, Collections.<Concept>emptySet()));
-            levels = result.numberOfLevels()-2;
+        do {
+            // Generate the match network, assuming that the selected operation is invokable
+            graph = fs.search(new SignatureIO<Concept>(inputs, Collections.<Concept>emptySet()));
+            levels = graph.numberOfLevels()-2;
+            Map<Operation<Concept>, Set<Concept>> unmatchedInputMap = fs.getUnmatchedInputMap();
+
+            int minSize = Integer.MAX_VALUE;
+            Operation<Concept> op = null;
+            // Find the operation with less unmatched inputs (different from 0)
+            for(Map.Entry<Operation<Concept>, Set<Concept>> entry : unmatchedInputMap.entrySet()){
+                int currentSize = entry.getValue().size();
+                if (currentSize != 0 && currentSize < minSize){
+                    minSize = currentSize;
+                    op = entry.getKey();
+                }
+            }
+            Set<Concept> newInputs = new HashSet<Concept>();
+            if (op == null){
+                // Select a random operation
+                Set<Operation<Concept>> invokableOps =
+                        Sets.newHashSet(graph.getOperations());
+                reachableOps.removeAll(invokableOps);
+                if (reachableOps.isEmpty()) break;
+                op = randomElement(reachableOps, r);
+                Set<Concept> generatedOutputs = Operations.outputs(invokableOps);
+                Set<Concept> matched = matcher.partialMatch(generatedOutputs, op.getSignature().getInputs()).getTargetElements();
+                newInputs.addAll(new HashSet<Concept>(Sets.difference(op.getSignature().getInputs(), matched)));
+            } else {
+                newInputs.addAll(unmatchedInputMap.get(op));
+            }
+
+            // Compute the minimum number of inputs that we need to add in order to make the
+            // operation invokable:
+            inputs.addAll(newInputs);
+        } while (levels < minLevels);
+
+        // Select N random outputs
+        Set<Concept> availableOutputs = Operations.outputs(graph.getOperations());
+        Set<Concept> outputs = new HashSet<Concept>();
+
+        float p = 2f;
+        int total = Math.round((p/100f) * availableOutputs.size()) + 1;
+        cli.println("Selecting " + total + " inputs...");
+        for(int i=0; i < total; i++){
+            outputs.add(randomElement(availableOutputs, r));
         }
-        cli.println(result.getLeveledList());
-        // Now, search for an optimal composition. Select a random op from each level
-        Set<Concept> requiredOutputs = new HashSet<Concept>();
-        for(int i=1; i < result.numberOfLevels()-1; i++){
-            Set<Operation<Concept>> ops = result.getOperationsAtLevel(i);
-            Operation<Concept> op = randomElement(ops, r);
-            // Select a random output
-            Concept output = randomElement(op.getSignature().getOutputs(), r);
-            if (r.nextBoolean()) requiredOutputs.add(output);
-        }
+
         // Generate optimal composition
-        cli.println("Selected inputs: " + inputs);
-        cli.println("Selected outputs: " + requiredOutputs);
+        cli.println("Total inputs: " + inputs.size());
+        cli.println("Total outputs: " + outputs.size());
+
         ComposIT<Concept, Boolean> composit = new ComposIT<Concept, Boolean>(dataset.getDefaultCompositionProblem());
         composit.addOptimization(new BackwardMinimizationOptimizer<Concept, Boolean>());
         composit.addOptimization(new FunctionalDominanceOptimizer<Concept, Boolean>());
-        composit.search(new SignatureIO<Concept>(inputs, requiredOutputs));
+        composit.search(new SignatureIO<Concept>(inputs, outputs));
+
+    }
+
+    private static <E> Set<E> randomSetWithRepetition(Collection<E> elements, float percentage, Random r){
+        int total = Math.round((percentage/100f) * elements.size()) + 1;
+        Set<E> set = new HashSet<E>();
+        for(int i=0; i < total; i++){
+            set.add(randomElement(elements, r));
+        }
+        return set;
     }
 
     private static <E> E randomElement(Iterator<E> it, Random r){
--- composit-core/src/main/java/es/usc/citius/composit/core/composition/search/ForwardServiceDiscoverer.java
+++ composit-core/src/main/java/es/usc/citius/composit/core/composition/search/ForwardServiceDiscoverer.java
@@ -44,15 +44,15 @@ public class ForwardServiceDiscoverer<E, T extends Comparable<T>> {
     private static final Logger log = LoggerFactory.getLogger(ForwardServiceDiscoverer.class);
     private InputDiscoverer<E> discovery;
     private SetMatchFunction<E, T> matcher;
+    private boolean relaxedMatchCondition = false;
+    private Map<Operation<E>, Set<E>> unmatchedInputMap = new HashMap<Operation<E>, Set<E>>();
 
     public ForwardServiceDiscoverer(InputDiscoverer<E> discovery, SetMatchFunction<E, T> matcher) {
         this.discovery = discovery;
         this.matcher = matcher;
     }
 
-
     public ServiceMatchNetwork<E,T> search(Signature<E> signature){
-        // Instrumentation for input discovery
         Set<E> availableInputs = new HashSet<E>(signature.getInputs());
         Set<E> newOutputs = new HashSet<E>(signature.getInputs());
         Set<E> unmatchedOutputs = new HashSet<E>(signature.getOutputs());
@@ -81,18 +81,29 @@ public class ForwardServiceDiscoverer<E, T extends Comparable<T>> {
                     unmatchedInputs = candidate.getSignature().getInputs();
                 }
                 // Check if the new concepts match some unmatched inputs
-                //Set<Resource> matched = matcher.matched(newOutputs, unmatchedInputs);
                 Set<E> matched = matcher.partialMatch(newOutputs, unmatchedInputs).getTargetElements();
-                // Update the unmatchedInputs
-                unmatchedInputs = Sets.newHashSet(Sets.difference(unmatchedInputs, matched));
-                unmatchedInputMap.put(candidate, unmatchedInputs);
-                // If there are no unmatched inputs, the service is invokable!
-                if (!unmatchedInputs.isEmpty()){
-                    it.remove();
+
+                // Don't check invokability
+                if (relaxedMatchCondition){
+                    // Remove only if there is no match at all
+                    if (matched.isEmpty()){
+                        it.remove();
+                    } else {
+                        boolean isNew = usedServices.add(candidate);
+                        if (!isNew) it.remove();
+                    }
                 } else {
-                    // Invokable operation, check if it was used previously
-                    boolean isNew = usedServices.add(candidate);
-                    if (!isNew) it.remove();
+                    // Update the unmatchedInputs
+                    unmatchedInputs = Sets.newHashSet(Sets.difference(unmatchedInputs, matched));
+                    unmatchedInputMap.put(candidate, unmatchedInputs);
+                    // If there are no unmatched inputs, the service is invokable!
+                    if (!unmatchedInputs.isEmpty()){
+                        it.remove();
+                    } else {
+                        // Invokable operation, check if it was used previously
+                        boolean isNew = usedServices.add(candidate);
+                        if (!isNew) it.remove();
+                    }
                 }
             }
             log.info("\t + [{}] operations selected for this level in {}: {}", candidates.size(), levelTimer.toString(), candidates);
@@ -130,6 +141,19 @@ public class ForwardServiceDiscoverer<E, T extends Comparable<T>> {
         log.info(" > Service match network computed in {}", networkWatch.stop().toString());
         log.info("Service Match Network created with {} levels (including source and sink) and {} operations.", leveledOps.size(), matchNetwork.listOperations().size());
         log.info("Forward Discovery done in {}", timer.toString());
+        this.unmatchedInputMap = unmatchedInputMap;
         return matchNetwork;
+    }
+
+    public boolean isRelaxedMatchCondition() {
+        return relaxedMatchCondition;
+    }
+
+    public void setRelaxedMatchCondition(boolean relaxedMatchCondition) {
+        this.relaxedMatchCondition = relaxedMatchCondition;
+    }
+
+    public Map<Operation<E>, Set<E>> getUnmatchedInputMap() {
+        return unmatchedInputMap;
     }
 }
