11228952f87e31c45ac224a98af55fa631e20f60
pablo.rodriguez.mier <pablo.rodriguez.mier@usc.es>
2014-04-09 11:27:43 +0200

    added script to automatically deploy javadoc to gh-pages. travis.yml updated.

---
+++ .config/publish-javadoc.sh
@@ -0,0 +1,30 @@
+#!/bin/bash
+
+if [ "$TRAVIS_REPO_SLUG" == "citiususc/composit" ] && [ "$TRAVIS_JDK_VERSION" == "oraclejdk7" ] && [ "$TRAVIS_PULL_REQUEST" == "false" ]; then
+
+  FOLDER=$TRAVIS_BRANCH
+  
+  if [ "$FOLDER" = "master" ]; then
+    FOLDER="latest"
+  fi
+  
+  echo -e "Deploying [$FOLDER] ComposIT JavaDoc to GitHub gh-pages\n"
+
+  # Copy the build folder with the javadoc to the corresponding folder
+  cp -R build/citiususc/composit/target/apidocs/ $HOME/javadoc/$FOLDER/
+
+  # Config git with the travis account
+  git config --global user.email "travis@travis-ci.org"
+  git config --global user.name "travis-ci"
+  
+  cd $HOME
+  git clone --quiet --branch=gh-pages https://${GH_TOKEN}@github.com/citiususc/composit gh-pages > /dev/null
+  cd gh-pages
+  rm -rf ./javadoc/$FOLDER/
+  cp -R $HOME/javadoc/$FOLDER ./javadoc/$FOLDER
+  git add .
+  git commit -a -m "auto-commit $TRAVIS_BRANCH ComposIT JavaDoc (build $TRAVIS_BUILD_NUMBER)"
+  git push -q origin gh-pages > /dev/null
+  echo -e "Published $TRAVIS_BRANCH JavaDoc to gh-pages.\n"
+  
+fi 
