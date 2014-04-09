514fdc97ab9a456e223dfb67bec4bca7ab4dc725
pablo.rodriguez.mier <pablo.rodriguez.mier@usc.es>
2014-04-09 18:48:10 +0200

    added config to deploy snapshots to the internal nexus

---
+++ .config/deploy-artifacts.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+echo "Auto-deploying artifact..."
+echo "Current branch: $TRAVIS_BRANCH"
+
+if [ "$TRAVIS_REPO_SLUG" == "citiususc/composit" ] && [ "$TRAVIS_JDK_VERSION" == "oraclejdk7" ] && [ "$TRAVIS_PULL_REQUEST" == "false" ] && [ "$TRAVIS_BRANCH" == "development" ]; then
+
+  mvn deploy --settings maven-settings.xml
+
+fi
