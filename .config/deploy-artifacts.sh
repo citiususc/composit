86b2303c7e1d97a44f277559b239b02a27d07e8e
Pablo Rodríguez Mier <pablo.rodriguez.mier@usc.es>
2014-04-09 22:29:24 +0200

    fixed maven-settings.xml path in deploy-artifacts.sh

--- .config/deploy-artifacts.sh
+++ .config/deploy-artifacts.sh
@@ -5,7 +5,7 @@ echo "Current branch: $TRAVIS_BRANCH"
 
 if [ "$TRAVIS_REPO_SLUG" == "citiususc/composit" ] && [ "$TRAVIS_JDK_VERSION" == "oraclejdk7" ] && [ "$TRAVIS_PULL_REQUEST" == "false" ] && [ "$TRAVIS_BRANCH" == "master" ]; then
   echo "Running mvn deploy..."
-  mvn deploy --settings maven-settings.xml
+  mvn deploy --settings .config/maven-settings.xml
 fi
 
 echo "Deployment script finished"
