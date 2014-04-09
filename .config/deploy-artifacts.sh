8bd05b02eb30ba407ecbe737ef390dd54d6de694
Pablo Rodríguez Mier <pablo.rodriguez.mier@usc.es>
2014-04-09 21:46:23 +0200

    secure vars updated. Deployment script minor changes

--- .config/deploy-artifacts.sh
+++ .config/deploy-artifacts.sh
@@ -4,7 +4,8 @@ echo "Auto-deploying artifact..."
 echo "Current branch: $TRAVIS_BRANCH"
 
 if [ "$TRAVIS_REPO_SLUG" == "citiususc/composit" ] && [ "$TRAVIS_JDK_VERSION" == "oraclejdk7" ] && [ "$TRAVIS_PULL_REQUEST" == "false" ] && [ "$TRAVIS_BRANCH" == "development" ]; then
-
+  echo "Running mvn deploy..."
   mvn deploy --settings maven-settings.xml
-
 fi
+
+echo "Deployment script finished"
