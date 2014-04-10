3fad649972ae76509b6188d06868615229435298
pablo.rodriguez.mier <pablo.rodriguez.mier@usc.es>
2014-04-10 10:08:39 +0200

    deploy-artifacts.sh fixed

--- .config/deploy-artifacts.sh
+++ .config/deploy-artifacts.sh
@@ -5,7 +5,9 @@ echo "Current branch: $TRAVIS_BRANCH"
 
 if [ "$TRAVIS_REPO_SLUG" == "citiususc/composit" ] && [ "$TRAVIS_JDK_VERSION" == "oraclejdk7" ] && [ "$TRAVIS_PULL_REQUEST" == "false" ] && [ "$TRAVIS_BRANCH" == "master" ]; then
   echo "Running mvn deploy..."
-  mvn deploy:deploy --settings .config/maven-settings.xml
+  mvn deploy --settings .config/maven-settings.xml -DskipTests=true
+else
+  echo "Skipping deployment for this build..."
 fi
 
-echo "Deployment script finished"
+echo "Deployment script finished."
